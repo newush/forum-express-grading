f9d7f3b092c9e00d8c9619aa2c4b3e520f93add3
newush <ushneway@gmail.com>
2021-02-01 00:04:29 -0500

    Initialize project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('handlebars', handlebars()) //新增樣板引擎
+app.set('view engine', 'handlebars') //使用樣板引擎
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
