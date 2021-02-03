cd8132e99da6a68e29db10da98b1494fed40dccf
newush <ushneway@gmail.com>
2021-02-02 23:04:31 -0500

    feat: add navbar to show login status

--- app.js
+++ app.js
@@ -17,16 +17,18 @@ app.listen(port, () => {
 // setup session and flash
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
-// 把 req.flash 放到 res.locals 裡面
-app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')
-  res.locals.error_messages = req.flash('error_messages')
-  next()
-})
 // setup passport
 app.use(passport.initialize())
 app.use(passport.session())
 
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  console.log('user', req.user)
+  next()
+})
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app, passport)
 
