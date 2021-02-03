1920d62dcd97be5664549bdeb305ba590c91469a
newush <ushneway@gmail.com>
2021-02-02 23:25:11 -0500

    feat: add authentication before rendering pages

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
   res.locals.user = req.user
-  console.log('user', req.user)
   next()
 })
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
