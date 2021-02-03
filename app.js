db0a87bbfaaab9abf671fc98f3c17e4792c44db2
newush <ushneway@gmail.com>
2021-02-02 20:46:17 -0500

    feat: add userController and signup page

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models') // 引入資料庫
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) //新增樣板引擎
 app.set('view engine', 'handlebars') //使用樣板引擎
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 module.exports = (app) => {
   //如果使用者訪問首頁，就導向 /restaurants 的頁面
   app.get('/', (req, res) => res.redirect('/restaurants'))
@@ -9,4 +10,6 @@ module.exports = (app) => {
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
   app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
