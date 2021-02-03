d3faccbd26974f3e6accea5fc49baad21c17e77e
newush <ushneway@gmail.com>
2021-02-03 17:32:09 -0500

    revise restaurant layout

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,11 +3,10 @@ const Restaurant = db.Restaurant
 
 const adminController = {
   getRestaurants: (req, res) => {
-    getRestaurants: (req, res) => {
-      return Restaurant.findAll({ raw: true }).then(restaurants => {
-        return res.render('admin/restaurants', { restaurants: restaurants })
-      })
-    }
+    return Restaurant.findAll({ raw: true }).then(restaurants => {
+      return res.render('admin/restaurants', { restaurants: restaurants })
+    })
   }
 }
+
 module.exports = adminController
