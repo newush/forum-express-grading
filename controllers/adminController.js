d4119aaf0e65e71fe24a6cf71b04689d92c9a9b7
newush <ushneway@gmail.com>
2021-02-09 19:43:08 -0500

    feat: add categories selector on admin create page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -16,7 +16,12 @@ const adminController = {
     })
   },
   createRestaurant: (req, res) => {
-    return res.render('admin/create')
+    Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      return res.render('admin/create', { categories: categories })
+    })
   },
   postRestaurant: (req, res) => {
     if (!req.body.name) {
