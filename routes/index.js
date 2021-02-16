fb1e359e54fce23f533d9fbf29457711eea0cc30
newush <ushneway@gmail.com>
2021-02-15 23:41:16 -0500

    feat: add feeds page

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ module.exports = (app, passport) => {
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   //在 /restaurants 底下則交給 restController.getRestaurants 來處理
   app.get('/restaurants', authenticated, restController.getRestaurants)
+  app.get('/restaurants/feeds', authenticated, restController.getFeeds)
   app.get('/restaurants/:id', authenticated, restController.getRestaurant)
   app.post('/comments', authenticated, commentController.postComment)
   app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
