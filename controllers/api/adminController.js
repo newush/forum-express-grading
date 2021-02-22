b52e5788a5892fda73ef03174b53198019749397
newush <ushneway@gmail.com>
2021-02-22 12:08:57 -0500

    refactor: adminController getRestaurant

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -1,10 +1,18 @@
 const db = require('../../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const User = db.User
+const imgur = require('imgur')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const adminService = require('../../services/adminService.js')
 const adminController = {
   getRestaurants: (req, res) => {
     adminService.getRestaurants(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  getRestaurant: (req, res) => {
+    adminService.getRestaurant(req, res, (data) => {
       return res.json(data)
     })
   }
--- services/adminService.js
+++ services/adminService.js
@@ -11,6 +11,12 @@ const adminService = {
     }).then(restaurants => {
       callback({ restaurants: restaurants })
     })
+  },
+  getRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, { include: [Category] }).then(restaurant => {
+      callback({ restaurant: restaurant })
+    })
   }
 }
+
 module.exports = adminService
