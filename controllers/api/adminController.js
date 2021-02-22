7043e4778eb3f6ec8413a02a5b52f75703b437d4
newush <ushneway@gmail.com>
2021-02-22 16:31:40 -0500

    refactor: adminController deleteRestaurant

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -15,6 +15,11 @@ const adminController = {
     adminService.getRestaurant(req, res, (data) => {
       return res.json(data)
     })
+  },
+  deleteRestaurant: (req, res) => {
+    adminService.deleteRestaurant(req, res, (data) => {
+      return res.json(data)
+    })
   }
 }
 module.exports = adminController
